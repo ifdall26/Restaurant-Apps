c254e9a33738322182709dd491db2c8d18bce531
ifdall26 <lisyukri.ifdal@gmail.com>
2024-04-19 21:53:47 +0700

    burger button gagal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,17 @@
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.css";
 
-console.log('Hello Coders! :)');
+console.log("Hello Coders! :)");
+
+// index.js
+
+// Burger Menu Functionality
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+
+  // Burger Animation
+  burger.classList.toggle("toggle");
+});
