8ff228861c5caab32ba257e2e827adf92093f1b7
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-01 13:21:12 +0700

    membuat fungsi cachingAppShell, deleteOldShell, dan revalidateCache

--- src/scripts/utils/cache-helper.js
+++ src/scripts/utils/cache-helper.js
@@ -1,6 +1,19 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-empty-function */
+/* eslint-disable linebreak-style */
 const CacheHelper = {
+  async cachingAppShell(requests) {
 
+  },
+
+  async deleteOldCache() {
+
+  },
+
+  async revalidateCache(request) {
+
+  },
 };
 
 export default CacheHelper;
