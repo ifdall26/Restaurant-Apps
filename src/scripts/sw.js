f2c44d8bcb3b15613f3ca3ce01e1cc7386ab773e
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-01 13:41:50 +0700

    mendaftarkan seluruh aset statis

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,8 +1,32 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable linebreak-style */
+import CacheHelper from './utils/cache-helper';
+ 
+// Daftar asset yang akan di-caching
+const assetsToCache = [
+  './',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  './images/heros/hero-image_2.jpg',
+  './images/bw_icon.png',
+  './images/catalog_logo.png',
+  './index.html',
+  './favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
 self.addEventListener('install', (event) => {
   console.log('Installing Service Worker ...');
 
