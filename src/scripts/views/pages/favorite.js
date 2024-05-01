18f88464e87612a0f11465ff8828597c5c73f177
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-02 00:39:41 +0700

    membuat fungsi after render untuk halaman fav

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,14 +1,28 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable import/named */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable linebreak-style */
+import FavoriteRestoIdb from '../../data/favorite-resto-idb';
+import { createRestoItemTemplate } from '../templates/template-creator';
+
 const Favorite = {
   async render() {
     return `
       <h2>Restoran Favoritmu:)</h2>
       <hr>
+      <div id="restaurant-list"></div>
     `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const restaurants = await FavoriteRestoIdb.getAllRestos();
+    const restaurantContainer = document.querySelector('#restaurants');
+    
+    restaurants.forEach((resto) => {
+      restaurantContainer.innerHTML += createRestoItemTemplate(resto);
+    });
   },
 };
 
