08bedff63d412db860f72030784c91e51a302f82
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-01 13:35:22 +0700

    import cache-helper

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: 'restoCache',
+  CACHE_NAME: 'JelajahRasa-V1',
   DATABASE_NAME: 'restoDB',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
