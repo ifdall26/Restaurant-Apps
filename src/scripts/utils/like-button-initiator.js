b90b50936c1e832a2e165e61e9e4c0fc52e1cbcd
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-01 23:52:46 +0700

    method method untuk like button

--- src/scripts/utils/like-button-initiator.js
+++ src/scripts/utils/like-button-initiator.js
@@ -1,12 +1,54 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-empty-function */
 /* eslint-disable linebreak-style */
 /* eslint-disable no-trailing-spaces */
+import FavoriteRestoIdb from '../data/favorite-resto-idb';
+import { createLikeButtonTemplate, createLikedButtonTemplate } from '../views/templates/template-creator';
+ 
 const LikeButtonInitiator = {
-  async init({ likeButtonContainer, movie }) {
+  async init({ likeButtonContainer, resto }) {
     this._likeButtonContainer = likeButtonContainer;
-    this._movie = movie;
+    this._resto = resto;
+
+    await this._renderButton();
+  },
+ 
+  async _renderButton() {
+    const { id } = this._resto;
+ 
+    if (await this._isRestoExist(id)) {
+      this._renderLiked();
+    } else {
+      this._renderLike();
+    }
+  },
+
+  async _isRestoExist(id) {
+    const resto = await FavoriteRestoIdb.getResto(id);
+    return !!resto;
+  },
+
+  _renderLike() {
+    this._likeButtonContainer.innerHTML = createLikeButtonTemplate();
+ 
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await FavoriteRestoIdb.putMovie(this._resto);
+      this._renderButton();
+    });
+  },
+ 
+  _renderLiked() {
+    this._likeButtonContainer.innerHTML = createLikedButtonTemplate();
+ 
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await FavoriteRestoIdb.deleteMovie(this._resto.id);
+      this._renderButton();
+    });
   },
 };
  
