b2f346e7da2ccc5fe07d60ae807acaf6b54a3efa
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-01 21:59:09 +0700

    masih belum bisa diakses offline

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,19 +1,15 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-console */
-/* eslint-disable linebreak-style */
 import { Workbox } from 'workbox-window';
- 
+
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
     console.log('Service Worker not supported in the browser');
     return;
   }
- 
-  const wb = new Workbox('./sw.bundle.js');
- 
+
+  const wb = new Workbox('/sw.bundle.js');
+
   try {
     await wb.register();
     console.log('Service worker registered');
--- webpack.common.js
+++ webpack.common.js
@@ -4,11 +4,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { GenerateSW } = require('workbox-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
+    styles: path.resolve(__dirname, 'src/styles/main.css'),
   },
   output: {
     filename: '[name].bundle.js',
@@ -38,9 +39,24 @@ module.exports = {
         },
       ],
     }),
-    new GenerateSW({
-      swDest: 'sw.bundle.js', // Nama file service worker yang akan dihasilkan
-      // Konfigurasi WorkboxWebpackPlugin lainnya
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://api.themoviedb.org/3/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'themoviedb-api',
+          },
+        },
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://image.tmdb.org/t/p/w500/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'themoviedb-image-api',
+          },
+        },
+      ],
     }),
   ],
 };
