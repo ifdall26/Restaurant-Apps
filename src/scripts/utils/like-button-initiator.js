3442942e503b519b943f8a818836663bff2aab85
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-01 23:34:48 +0700

    membuat like-button-initiator.js

---
+++ src/scripts/utils/like-button-initiator.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-empty-function */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+const LikeButtonInitiator = {
+  async init({ likeButtonContainer, movie }) {
+    this._likeButtonContainer = likeButtonContainer;
+    this._movie = movie;
+  },
+};
+ 
+export default LikeButtonInitiator;
