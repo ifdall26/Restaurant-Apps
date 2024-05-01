543c0798948f01b24464ddf4d30d926419471d91
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-01 22:55:49 +0700

    CRUD untuk favorite resto

--- src/scripts/data/favorite-resto-idb.js
+++ src/scripts/data/favorite-resto-idb.js
@@ -13,3 +13,18 @@ const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
     database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
   },
 });
+
+const FavoriteRestoIdb = {
+  async getResto(id) {
+    return (await dbPromise).get(OBJECT_STORE_NAME, id);
+  },
+  async getAllRestos() {
+    return (await dbPromise).getAll(OBJECT_STORE_NAME);
+  },
+  async putResto(resto) {
+    return (await dbPromise).put(OBJECT_STORE_NAME, resto);
+  },
+  async deleteResto(id) {
+    return (await dbPromise).delete(OBJECT_STORE_NAME, id);
+  },
+};
