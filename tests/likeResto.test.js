526104c11e36b21dc017f86e361e65593ee87231
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-14 00:45:20 +0700

    mangatasi kode yang muncul berulang

--- tests/likeResto.test.js
+++ tests/likeResto.test.js
@@ -7,6 +7,10 @@ describe('Liking A Resto', () => {
   it('should show the like button when the restaurant has not been liked before', async () => {
     document.body.innerHTML = '<div id="likeButtonContainer"></div>';
 
+    beforeEach(() => {
+      addLikeButtonContainer();
+    });
+
     await LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
       restaurant: {
@@ -18,7 +22,6 @@ describe('Liking A Resto', () => {
   });
 
   it('should not show the unlike button when the restaurant has not been liked before', async () => {
-    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
     await LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
       restaurant: {
@@ -29,7 +32,6 @@ describe('Liking A Resto', () => {
   });
 
   it('should be able to like the restaurant', async () => {
-    document.body.innerHTML = '<div id="likeButtonContainer"></div>';
     await LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
       restaurant: {
