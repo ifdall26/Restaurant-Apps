b97b4937596550f42798b822e22895b3cd59b15e
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-14 01:07:48 +0700

    tidak ada id ganda dan tanpa id

--- tests/likeResto.test.js
+++ tests/likeResto.test.js
@@ -4,13 +4,15 @@ import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
 import FavoriteRestoIdb from '../src/scripts/data/favorite-resto-idb';
 
 describe('Liking A Resto', () => {
-  it('should show the like button when the restaurant has not been liked before', async () => {
+  const addLikeButtonContainer = () => {
     document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+  };
 
-    beforeEach(() => {
-      addLikeButtonContainer();
-    });
+  beforeEach(() => {
+    addLikeButtonContainer();
+  });
 
+  it('should show the like button when the restaurant has not been liked before', async () => {
     await LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
       restaurant: {
@@ -45,6 +47,32 @@ describe('Liking A Resto', () => {
     const restaurant = await FavoriteRestoIdb.getResto('rqdv5juczeskfw1e867');
     expect(restaurant).toEqual({ id: 'rqdv5juczeskfw1e867' });
 
-    await FavoriteRestoIdb.deleteResto(1);
+    await FavoriteRestoIdb.deleteResto('rqdv5juczeskfw1e867');
+  });
+
+  it('should not add a restaurant again when its already liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: 'rqdv5juczeskfw1e867',
+      },
+    });
+    // Tambahkan restaurant dengan ID 1 ke daftar restaurant yang disukai
+    await FavoriteRestoIdb.putResto({ id: 'rqdv5juczeskfw1e867' });
+    // Simulasikan pengguna menekan tombol suka film
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    // Tidak ada restaurant yang ganda
+    expect(await FavoriteRestoIdb.getAllRestos()).toEqual([{ id: 'rqdv5juczeskfw1e867' }]);
+    await FavoriteRestoIdb.deleteResto('rqdv5juczeskfw1e867');
+  });
+
+  it('should not add a restaurant when it has no id', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {},
+    });
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+    expect(await FavoriteRestoIdb.getAllRestos()).toEqual([]);
   });
 });
