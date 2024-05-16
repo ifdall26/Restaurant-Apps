6f2728a52233b19a6c83c0b0c2f17c49e81efa3b
ifdall26 <lisyukri.ifdal@gmail.com>
2024-05-16 14:54:51 +0700

    jangan sampai salah nama

--- e2e/Liking_Restos.test.js
+++ e2e/Liking_Restos.test.js
@@ -10,17 +10,22 @@ Scenario('showing empty liked restaurants', ({ I }) => {
   I.see('Tidak ada Restoran yang Ditampilkan', '.resto-nothing');
 });
 
-Scenario('liking one restaurants', ({ I }) => {
+const assert = require('assert');
+
+Scenario('liking one restaurants', async ({ I }) => {
   I.see('Tidak ada Restoran yang Ditampilkan', '.resto-nothing');
   I.amOnPage('/');
 
-  I.seeElement('.restaurant a');
-  I.click(locate('.restaurant a').first());
+  const firstRestoTitle = await I.grabTextFrom('//*[@id="restaurant-list"]/a/div/h2');
+  I.click('//*[@id="restaurant-list"]/a/div/h2');
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/favorite');
   I.seeElement('.restaurant');
+  const likedRestoTitle = await I.grabTextFrom('.restaurant h2');
+
+  assert.strictEqual(firstRestoTitle, likedRestoTitle);
   pause();
 });
